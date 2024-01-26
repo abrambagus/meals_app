62c07dae1aab46ecea6c90fdafeae8858dbda7b6
abram <abrambagus@gmail.com>
2024-01-26 17:31:21 +0700

    add list category screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -10,33 +12,16 @@ class CategoriesScreen extends StatelessWidget {
         title: const Text("Pick your category"),
       ),
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
-        children: const [
-          Text(
-            "A",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "A",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "A",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "A",
-            style: TextStyle(color: Colors.white),
-          ),
-          Text(
-            "A",
-            style: TextStyle(color: Colors.white),
-          ),
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
         ],
       ),
     );
